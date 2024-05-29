be00fe4b99463479ccef9d3bdc7a60417c404d4a
hkd <hkinnovapath@gmail.com>
2024-05-29 23:48:39 +0530

    updated logo

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export",
-  // basePath: "/whiteboxLearning-wbl",
-  // images: {
-  //   unoptimized: true,
-  // },
+  output: "export",
+  basePath: "/whiteboxLearning-wbl",
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;

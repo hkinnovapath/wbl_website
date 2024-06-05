f4c5a2b80e219bc94740d271c752c17a4b10d90a
hkd <hkinnovapath@gmail.com>
2024-06-06 04:18:07 +0530

    updated shedule res

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -103,11 +103,11 @@ export default function Schedule() {
   return (
     <div className="min-h-screen">
       {/* Main content */}
-      <main className="mx-auto max-w-6xl px-4 py-6 sm:px-6 lg:px-8">
+      <main className="mx-auto max-w-6xl px-4  sm:px-6 lg:px-8">
         {/* Navbar */}
 
         <nav className="mt-16 flex flex-col items-start justify-between sm:flex-row sm:items-center">
-          <h1 className="pt-10 text-center text-2xl font-bold sm:pt-0 sm:text-3xl lg:text-4xl ">Schedule</h1>
+          <h1 className="pt-10  sm:text-start text-center  w-full text-2xl font-bold sm:pt-0 sm:text-3xl lg:text-4xl ">Schedule</h1>
         {/* <nav className="mt-16 flex-none sm:flex sm:items-center sm:justify-between  "> */}
           {/* <h1 className="text-2xl font-bold sm:text-3xl lg:text-4xl">Schedule</h1> */}
           <div className="hidden sm:block">
@@ -118,15 +118,15 @@ export default function Schedule() {
         {/* Section with buttons and dropdowns */}
         <section className="mb-8 flex flex-col sm:flex-row sm:justify-between">
           {/* Left side */}
-          <div className="mt-10 flex flex-col sm:w-1/6">
+          <div className="mt-10 flex sm:justify-start justify-center sm:gap-0 gap-5  flex-row  sm:flex-col sm:w-1/6">
             <button
-              className="mb-2 rounded-md bg-blue-500 bg-gradient-to-br from-primary to-blue-300 px-4 py-2 text-sm font-bold text-black hover:bg-blue-600 hover:bg-gradient-to-tl hover:from-primary hover:to-blue-300 sm:text-base lg:text-lg"
+              className="mb-2 sm:w-full w-2/5 rounded-md bg-blue-500 bg-gradient-to-br from-primary to-blue-300 px-4 py-2 text-sm font-bold text-black hover:bg-blue-600 hover:bg-gradient-to-tl hover:from-primary hover:to-blue-300 sm:text-base lg:text-lg"
               onClick={() => handleButtonClick("calendar")}
             >
               Calendar
             </button>
             <button
-              className="mb-2  rounded-md bg-blue-500 bg-gradient-to-br from-primary to-blue-300 px-4 py-2 text-sm font-bold text-black hover:bg-blue-600 hover:bg-gradient-to-tl hover:from-primary hover:to-blue-300 sm:text-base lg:text-lg"
+              className="mb-2  sm:w-full w-2/5 rounded-md bg-blue-500 bg-gradient-to-br from-primary to-blue-300 px-4 py-2 text-sm font-bold text-black hover:bg-blue-600 hover:bg-gradient-to-tl hover:from-primary hover:to-blue-300 sm:text-base lg:text-lg"
               onClick={() => handleButtonClick("courseContent")}
             >
               Course Content

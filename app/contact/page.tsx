916f04d6584d391cbe1e11551fc7929397715656
hkd <hkinnovapath@gmail.com>
2024-06-11 04:15:32 +0530

    updated contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -39,9 +39,9 @@ import ContactForm from "@/components/Contact/ContactForm";
 const ContactPage = () => {
   return (
     <>
-      <div className=" w-full mx-auto max-w-6xl px-4 py-6 sm:px-6 text-white ">
-        <nav className="py-6 mt-16 flex-none md:flex sm:items-center sm:justify-between ">
-          <h1 className="  text-center  text-2xl font-bold sm:py-0 sm:text-3xl lg:text-4xl  text-black dark:text-white">
+      <div className=" w-full mx-auto max-w-6xl px-4 pt-4 sm:px-6 text-white ">
+        <nav className="pt-6 mt-16 flex-none md:flex sm:items-center sm:justify-between ">
+          <h1 className="text-center text-2xl font-bold sm:py-0 sm:text-3xl lg:text-4xl  text-black dark:text-white">
             Contact Us
           </h1>
           <div className="hidden md:block">
@@ -49,17 +49,17 @@ const ContactPage = () => {
          </div>
         </nav>
         <div className="flex flex-wrap md:flex-nowrap">
-          <div className="w-full">
+          <div className="w-full ">
             <ContactForm />
           </div>
         </div>
-        <div>
-          <h1 className="mb-4 text-4xl font-bold text-black dark:text-white">
+        <div  >
+          <h1 className="text-center   sm:text-left  text-2xl font-bold  sm:text-3xl lg:text-4xl mb-4  text-black dark:text-white">
             How to find us
           </h1>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3156.692512341928!2d-121.91246142459563!3d37.703419816267946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fec05cb8a6a33%3A0x50cb7964adacbf25!2sWhitebox%20Learning%20Inc!5e0!3m2!1sen!2sin!4v1714515428412!5m2!1sen!2sin"
-            className="h-96 w-full rounded-lg shadow-2xl shadow-black brightness-90"
+            className="h-96 w-full mb-8 rounded-lg shadow-2xl  brightness-90"
             frameBorder="0"
             aria-hidden="false"
           ></iframe>
--- components/Contact/ContactForm.tsx
+++ components/Contact/ContactForm.tsx
@@ -2,34 +2,34 @@ import ContactDetails from "./ContactDetails";
 
 const Contact = () => {
   return (
-    <section id="contact" className="w-full overflow-hidden   pb-10 ">
-      <div className="container ">
-        {/* <div className="-mx-4 flex flex-wrap  "> */}
-        <div className="-mx-4 flex flex-wrap">
-          <div className="   w-full   px-4  lg:w-7/12 xl:w-8/12  ">
+    <section id="contact" className="w-full  overflow-hidden pb-16 ">
+      <div className="container">
+        {/* <div className="-mx-4 flex flex-wrap"> */}
+        <div className="-mx-4 flex flex-wrap ">
+          <div className="w-full  lg:w-7/12 xl:w-8/12 ">
             <div
-              className="wow   fadeInUp mb-12  rounded-2xl  py-2  px-8   lg:mb-5 lg:px-8"
+              className="wow fadeInUp  rounded-2xl"
               data-wow-delay=".15s
               "
             >
-              <div className="flex  min-h-screen flex-col justify-center py-6 sm:py-4">
-                <div className="relative  py-3 sm:mx-auto sm:max-w-xl">
+              <div className="flex min-h-screen flex-col  justify-start sm:justify-center py-6 sm:py-4">
+                <div className="relative py-3 sm:mx-auto sm:max-w-xl">
                   <div className="absolute inset-0 hidden -skew-y-6 transform bg-gradient-to-r from-indigo-300 to-purple-400 shadow-lg dark:bg-gradient-to-r dark:from-indigo-700 dark:to-purple-500 sm:-rotate-6 sm:skew-y-0 sm:rounded-3xl md:block"></div>
-                  <div className="relative  rounded-3xl bg-gradient-to-br from-pink-400 to-sky-200 p-14 py-10  text-white shadow-lg dark:bg-gradient-to-br dark:from-pink-700 dark:to-sky-500">
-                    <div className="pb-6  text-center">
-                      <h1 className="text-lg font-bold sm:text-2xl md:text-3xl text-black dark:text-white">
+                  <div className=" px-10 py-10 sm:p-16  relative rounded-3xl bg-gradient-to-br from-pink-400 to-sky-200   text-white shadow-lg dark:bg-gradient-to-br dark:from-pink-700 dark:to-sky-500">
+                    <div className="pb-6 text-center">
+                      <h1 className="text-lg font-bold  text-black dark:text-white sm:text-2xl md:text-3xl">
                         Get in touch!
                       </h1>
-                      <p className="text-gray-700 dark:text-gray-300 text-xs sm:text-sm md:text-md">
+                      <p className="md:text-md text-xs text-gray-700 dark:text-gray-300 sm:text-sm">
                         We&apos;d love to hear from you.
                       </p>
-                      <p className="text-gray-700 dark:text-gray-300 text-xs sm:text-sm md:text-md">
+                      <p className="md:text-md text-xs text-gray-700 dark:text-gray-300 sm:text-sm">
                         Fill up the form below to send us a message.
                       </p>
                     </div>
                     <form
                       action="#"
-                      className="text-xs sm:text-sm md:text-md text-black dark:text-white "
+                      className="md:text-md text-xs text-black dark:text-white sm:text-sm "
                       method="post"
                     >
                       <label htmlFor="name" className="mb-2 block">
@@ -68,14 +68,19 @@ const Contact = () => {
                         name="message"
                         style={{ height: "121px" }}
                       ></textarea>
-                      <div className="flex gap-5 justify-between">
+                      <div className="flex justify-between gap-3 sm:gap-5">
                         <input
-                          className="focus:shadow-outline w-36  rounded-lg bg-gradient-to-br  from-indigo-500  to-indigo-200 py-2 px-4 font-bold text-black hover:bg-indigo-700 hover:from-indigo-500 hover:to-indigo-200 focus:outline-none dark:text-white"
+                          className="
+                          md:text-md text-[11px] sm:text-sm
+                          
+                          focus:shadow-outline w-36  rounded-lg bg-gradient-to-br  from-indigo-500  to-indigo-200 p-1 sm:py-2 sm:px-4 px-1 font-bold text-black hover:bg-indigo-700 hover:from-indigo-500 hover:to-indigo-200 focus:outline-none dark:text-white"
                           type="submit"
                           value="Send ➤"
                         />
                         <input
-                          className="focus:shadow-outline w-36  rounded-lg bg-gradient-to-br from-red-500 to-red-200 py-2 px-4 font-bold text-black shadow-xl hover:bg-red-700 hover:bg-gradient-to-tl hover:from-red-500 hover:to-red-200  focus:outline-none dark:text-white"
+                          className="
+                          md:text-md text-[11px]  sm:text-sm
+                          focus:shadow-outline w-36  rounded-lg bg-gradient-to-br from-red-500 to-red-200 p-1 sm:py-2 sm:px-4 px-1 font-bold text-black shadow-xl hover:bg-red-700 hover:bg-gradient-to-tl hover:from-red-500 hover:to-red-200  focus:outline-none dark:text-white"
                           type="reset"
                         />
                       </div>
@@ -85,7 +90,7 @@ const Contact = () => {
               </div>
             </div>
           </div>
-          <div className="mx-auto  flex items-center justify-center  py-3 sm:mx-auto     sm:max-w-xl  lg:w-5/12 xl:w-4/12">
+          <div className="mx-auto  flex  items-center justify-center  py-3 sm:mx-auto  sm:max-w-xl  lg:w-5/12 xl:w-4/12">
             <ContactDetails />
           </div>
         </div>
@@ -111,7 +116,7 @@ export default Contact;
 //               <div className="flex flex-col justify-center py-6 sm:py-4">
 //                 <div className="relative py-3 sm:mx-auto sm:max-w-xl">
 //                   <div className="absolute inset-0 -skew-y-6 transform dark:bg-gradient-to-r dark:from-indigo-700 dark:to-purple-500 bg-gradient-to-r from-indigo-300 to-purple-400 shadow-lg sm:-rotate-6 sm:skew-y-0 sm:rounded-3xl"></div>
-//                   <div className="relative bg-gradient-to-br from-pink-400 to-sky-200 dark:bg-gradient-to-br dark:from-pink-700 dark:to-sky-500 px-4 py-10 text-white shadow-lg sm:rounded-3xl sm:p-20">
+//                   <div className="relative bg-gradient-to-br from-pink-400 to-sky-200 dark:bg-gradient-to-br dark:from-pink-700 dark:to-sky-500  text-white shadow-lg sm:rounded-3xl px-4 py-10 sm:p-20">
 //                     <div className="pb-6 text-center">
 //                       <h1 className="text-3xl dark:text-white text-black">Get in touch!</h1>
 //                       <p className="dark:text-gray-300 text-gray-700">

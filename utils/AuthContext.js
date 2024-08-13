7f032b362672a3d5073eb465b11264000b6c889f
Ameer <ameer.innovapath@gmail.com>
2024-08-13 15:55:24 -0700

    updated user_dashboard

--- utils/AuthContext.js
+++ utils/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState, useEffect } from 'react';
+import { createContext, useContext, useState, useEffect } from "react";
 
 const AuthContext = createContext();
 
@@ -6,17 +6,18 @@ export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   useEffect(() => {
-    const token = localStorage.getItem('access_token');
+    const token = localStorage.getItem("access_token");
     setIsAuthenticated(!!token);
   }, []);
 
   const login = (token) => {
-    localStorage.setItem('access_token', token);
+    localStorage.setItem("access_token", token);
     setIsAuthenticated(true);
   };
 
   const logout = () => {
-    localStorage.removeItem('access_token');
+    localStorage.removeItem("access_token");
+    sessionStorage.clear();
     setIsAuthenticated(false);
   };
 
@@ -28,4 +29,3 @@ export const AuthProvider = ({ children }) => {
 };
 
 export const useAuth = () => useContext(AuthContext);
-

236997b7db0ff3ba96f031d936c8a9345ae1ffd0
Manisai reddy <manisaisaduvala21@gmai.com>
2024-09-04 21:31:36 +0530

    modified the view

--- next.config.js
+++ next.config.js
@@ -47,6 +47,38 @@
 
 // module.exports = nextConfig;
 
+// const path = require('path');
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ["whitebox-learning.com"],
+//   },
+//   experimental: {
+//     missingSuspenseWithCSRBailout: false,
+//   },
+//   reactStrictMode: false,
+//   env: {
+//     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+//     RESUME_PUBLIC_API_URL: process.env.RESUME_PUBLIC_API_URL,
+//   },
+//   webpack: (config, { isServer }) => {
+//     config.module.rules.push({
+//       test: /\.hbs$/, // Handle .hbs files
+//       loader: "handlebars-loader", // Use handlebars-loader
+//       options: {
+//         partialDirs: [
+//           path.join(__dirname, 'public/templates/partials'), // Specify the directory for partials
+//         ],
+//       },
+//     });
+
+//     return config;
+//   },
+// };
+
+// module.exports = nextConfig;
+
 const path = require('path');
 
 /** @type {import('next').NextConfig} */
@@ -78,3 +110,4 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+

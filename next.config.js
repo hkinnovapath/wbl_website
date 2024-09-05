cc1f901a948f0bbe0b063b31f846ef21bb3b60f2
hkd <hkinnovapath@gmail.com>
2024-09-06 04:10:32 +0530

    updated next config for leads app

--- next.config.js
+++ next.config.js
@@ -10,6 +10,44 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL ,
   },
+  async rewrites() {
+    return [
+      {
+        //for wildcard routing use below but it will display nothing for no pages 
+        source: '/:path*',
+        destination: 'http://localhost:3001/:path*', // Update with your React app's port    
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ["whitebox-learning.com"],
+//   },
+//   experimental: {
+//     missingSuspenseWithCSRBailout: false,
+//   },
+//   reactStrictMode: false,
+//   env: {
+//     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+//   },
+//   async rewrites() {
+//     return [
+//       {
+//         //for wildcard routing use below but it will display nothing for no pages 
+//         source: '/:path*',
+//         destination: 'http://localhost:3001/:path*', // Update with your React app's port
+       
+         
+//     },
+//     ];
+//   },
+// };
+
+module.exports = nextConfig;
+

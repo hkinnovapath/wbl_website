93133cc56f76cab2b25325de4e0ad328b8c0fc07
vinayInnovapath <abhii.innovapath@gmail.com>
2024-09-10 05:11:06 +0530

    social-login

--- utils/AuthContext.js
+++ utils/AuthContext.js
@@ -1,3 +1,37 @@
+// import { createContext, useContext, useState, useEffect } from "react";
+
+// const AuthContext = createContext();
+
+// export const AuthProvider = ({ children }) => {
+//   const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+//   useEffect(() => {
+//     const token = localStorage.getItem("access_token");
+//     setIsAuthenticated(!!token);
+//   }, []);
+
+//   const login = (token) => {
+//     localStorage.setItem("access_token", token);
+//     setIsAuthenticated(true);
+//   };
+
+//   const logout = () => {
+//     localStorage.removeItem("access_token");
+//     sessionStorage.clear();
+//     setIsAuthenticated(false);
+//   };
+
+//   return (
+//     <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// };
+
+// export const useAuth = () => useContext(AuthContext);
+
+
+
 import { createContext, useContext, useState, useEffect } from "react";
 
 const AuthContext = createContext();
@@ -29,3 +63,4 @@ export const AuthProvider = ({ children }) => {
 };
 
 export const useAuth = () => useContext(AuthContext);
+

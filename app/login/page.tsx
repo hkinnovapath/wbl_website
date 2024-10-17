0aa6b69950b72ad2aa1a3bc867519550835c8d23
hkd <hkinnovapath@gmail.com>
2024-10-18 04:27:39 +0530

    updated code

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -12,6 +12,7 @@ const SigninPage = () => {
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(false);
+  const [gloading, setGLoading] = useState(false);
   const [responseStatus, setResponseStatus] = useState("");
   const [loggedIn, setLoggedIn] = useState(false);
   const { data: session, status } = useSession();
@@ -31,7 +32,7 @@ const SigninPage = () => {
   const handleGoogleSignIn = async () => {
     setGoogleMessage(""); // Clear previous messages at the start
     setGoogleStatus(""); // Clear previous status at the start
-    setLoading(true); // Set loading to true
+    setGLoading(true); // Set gloading to true
 
     // Call the signIn function and cast the response type
     const response: ExtendedSignInResponse | undefined = (await signIn(
@@ -42,16 +43,9 @@ const SigninPage = () => {
     if (!response) {
       // setGoogleMessage("No response received from Google sign-in.");
       setGoogleStatus("error");
-      setLoading(false); // Set loading to false
+      setGLoading(false); // Set gloading to false
       return;
     }
-
-    // if (response.error) {
-    //   setGoogleMessage("Failed to sign in with Google.");
-    //   setGoogleStatus("error");
-    //   setLoading(false); // Set loading to false
-    //   return;
-    // }
 
     // Handle success response
     if (response.url) {
@@ -68,10 +62,9 @@ const SigninPage = () => {
         setGoogleMessage("Logged in successfully!");
         setGoogleStatus("success");
 
-        // Redirect after a short delay
-        setTimeout(() => {
-          router.push(callbackUrl);
-        }, 500);
+        
+          router.push('/');
+        
       } else {
         setGoogleMessage("Unexpected status received: " + status);
         setGoogleStatus("error");
@@ -81,27 +74,23 @@ const SigninPage = () => {
       setGoogleStatus("error");
     }
 
-    setLoading(false); // Set loading to false at the end
+    setGLoading(false); // Set gloading to false at the end
   };
 
-  // useEffect to handle session changes
-  useEffect(() => {
-    // console.log(session?.user?.status);
 
-    if (session?.user?.status === "inactive") {
-      setGoogleMessage("Inactive account. Please contact admin.");
-      setGoogleStatus("error");
-    } else if (session?.user?.status === "registered") {
-      setGoogleMessage("Registered successfully !!");
-      setGoogleStatus("success");
-    } else if (session?.user?.status === "active") {
-      setGoogleMessage("Logged in successfully!");
-      setGoogleStatus("success");
-      setTimeout(() => {
-        router.push(callbackUrl);
-      }, 500);
-    }
-  }, [session, router, callbackUrl]);
+ // useEffect to handle session changes
+ useEffect(() => {
+  if (session?.user?.status === "inactive") {
+    setGoogleMessage("Inactive account. Please contact admin.");
+    setGoogleStatus("error");
+  } else if (session?.user?.status === "registered") {
+    setGoogleMessage("Registered successfully !!");
+    setGoogleStatus("success");
+  } else if (session?.user?.status === "active") {
+    setGoogleMessage("Logged in successfully!");
+    setGoogleStatus("success");
+    router.push('/');  }
+}, [session, router]);
 
   // Reset messages on component mount
   useEffect(() => {
@@ -188,68 +177,90 @@ const SigninPage = () => {
                   Sign In to your account
                 </p>                
                 <button
-                  className="dark:shadow-signUp mb-4 flex w-full items-center justify-center rounded-3xl bg-white py-2 px-5 text-sm font-medium text-primary shadow-one dark:bg-white dark:text-black sm:mb-6 sm:py-3 sm:text-base"
-                  onClick={handleGoogleSignIn}
-                  disabled={loading} // Disable button while loading
-                >
-                  {loading ? (
-                    <>
-                      <span className="mr-3 animate-spin">
-                        {/* Loading spinner */}
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="h-5 w-5"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                        >
-                          <path
-                            fillRule="evenodd"
-                            d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-14a1 1 0 00-2 0v4.586l-1.293-1.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 8.586V4z"
-                            clipRule="evenodd"
-                          />
-                        </svg>
-                      </span>
-                      Loading...
-                    </>
-                  ) : (
-                    <>
-                      <span className="mr-3">
-                        <svg
-                          width="20"
-                          height="20"
-                          viewBox="0 0 20 20"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <g clipPath="url(#clip0_95:967)">
-                            <path
-                              d="M20.0001 10.2216C20.0122 9.53416 19.9397 8.84776 19.7844 8.17725H10.2042V11.8883H15.8277C15.7211 12.539 15.4814 13.1618 15.1229 13.7194C14.7644 14.2769 14.2946 14.7577 13.7416 15.1327L13.722 15.257L16.7512 17.5567L16.961 17.5772C18.8883 15.8328 19.9997 13.266 19.9997 10.2216"
-                              fill="#4285F4"
-                            />
-                            <path
-                              d="M10.2042 20.0001C12.9592 20.0001 15.2721 19.1111 16.9616 17.5778L13.7416 15.1332C12.88 15.7223 11.7235 16.1334 10.2042 16.1334C8.91385 16.126 7.65863 15.7206 6.61663 14.9747C5.57464 14.2287 4.79879 13.1802 4.39915 11.9778L4.27957 11.9878L1.12973 14.3766L1.08856 14.4888C1.93689 16.1457 3.23879 17.5387 4.84869 18.512C6.45859 19.4852 8.31301 20.0005 10.2046 20.0001"
-                              fill="#34A853"
-                            />
-                            <path
-                              d="M4.39911 11.9777C4.17592 11.3411 4.06075 10.673 4.05819 9.99996C4.0623 9.32799 4.17322 8.66075 4.38696 8.02225L4.38127 7.88968L1.19282 5.4624L1.08852 5.51101C0.372885 6.90343 0.00012207 8.4408 0.00012207 9.99987C0.00012207 11.5589 0.372885 13.0963 1.08852 14.4887L4.39911 11.9777Z"
-                              fill="#FBBC05"
-                            />
-                            <path
-                              d="M10.2042 3.86663C11.6663 3.84438 13.0804 4.37803 14.1498 5.35558L17.0296 2.59996C15.1826 0.901848 12.7366 -0.0298855 10.2042 -3.6784e-05C8.3126 -0.000477834 6.45819 0.514732 4.8483 1.48798C3.2384 2.46124 1.93649 3.85416 1.08813 5.51101L4.38775 8.02225C4.79132 6.82005 5.56974 5.77231 6.61327 5.02675C7.6568 4.28118 8.91279 3.87541 10.2042 3.86663Z"
-                              fill="#EB4335"
-                            />
-                          </g>
-                          <defs>
-                            <clipPath id="clip0_95:967">
-                              <rect width="20" height="20" fill="white" />
-                            </clipPath>
-                          </defs>
-                        </svg>
-                      </span>
-                      Sign In with Google
-                    </>
-                  )}
-                </button>
+  className="dark:shadow-signUp mb-4 flex w-full items-center justify-center rounded-3xl bg-white py-2 px-5 text-sm font-medium text-primary shadow-one dark:bg-white dark:text-black sm:mb-6 sm:py-3 sm:text-base"
+  onClick={handleGoogleSignIn}
+  disabled={gloading} // Disable button while gloading
+>
+  {gloading ? (
+    <div className="flex items-center justify-center text-center   sm:text-2xl">
+      Loading
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        className="h-5 w-5 mr-2  sm:h-8 sm:w-8"
+        
+      >
+        <circle cx="4" cy="12" r="3" fill="currentColor">
+          <animate
+            id="svgSpinners3DotsScale0"
+            attributeName="r"
+            begin="0;svgSpinners3DotsScale1.end-0.2s"
+            dur="0.6s"
+            values="3;.2;3"
+          />
+        </circle>
+        <circle cx="12" cy="12" r="3" fill="currentColor">
+          <animate
+            attributeName="r"
+            begin="svgSpinners3DotsScale0.end-0.48s"
+            dur="0.6s"
+            values="3;.2;3"
+          />
+        </circle>
+        <circle cx="20" cy="12" r="3" fill="currentColor">
+          <animate
+            id="svgSpinners3DotsScale1"
+            attributeName="r"
+            begin="svgSpinners3DotsScale0.end-0.36s"
+            dur="0.6s"
+            values="3;.2;3"
+          />
+        </circle>
+      </svg>
+      
+    </div>
+  ) : (
+    <>
+      <span className="mr-3">
+        <svg
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          // className="h-5 w-5 sm:h-8 sm:w-8"
+        >
+          <g clipPath="url(#clip0_95:967)">
+            <path
+              d="M20.0001 10.2216C20.0122 9.53416 19.9397 8.84776 19.7844 8.17725H10.2042V11.8883H15.8277C15.7211 12.539 15.4814 13.1618 15.1229 13.7194C14.7644 14.2769 14.2946 14.7577 13.7416 15.1327L13.722 15.257L16.7512 17.5567L16.961 17.5772C18.8883 15.8328 19.9997 13.266 19.9997 10.2216"
+              fill="#4285F4"
+            />
+            <path
+              d="M10.2042 20.0001C12.9592 20.0001 15.2721 19.1111 16.9616 17.5778L13.7416 15.1332C12.88 15.7223 11.7235 16.1334 10.2042 16.1334C8.91385 16.126 7.65863 15.7206 6.61663 14.9747C5.57464 14.2287 4.79879 13.1802 4.39915 11.9778L4.27957 11.9878L1.12973 14.3766L1.08856 14.4888C1.93689 16.1457 3.23879 17.5387 4.84869 18.512C6.45859 19.4852 8.31301 20.0005 10.2046 20.0001"
+              fill="#34A853"
+            />
+            <path
+              d="M4.39911 11.9777C4.17592 11.3411 4.06075 10.673 4.05819 9.99996C4.0623 9.32799 4.17322 8.66075 4.38696 8.02225L4.38127 7.88968L1.19282 5.4624L1.08852 5.51101C0.372885 6.90343 0.00012207 8.4408 0.00012207 9.99987C0.00012207 11.5589 0.372885 13.0963 1.08852 14.4887L4.39911 11.9777Z"
+              fill="#FBBC05"
+            />
+            <path
+              d="M10.2042 3.86663C11.6663 3.84438 13.0804 4.37803 14.1498 5.35558L17.0296 2.59996C15.1826 0.901848 12.7366 -0.0298855 10.2042 -3.6784e-05C8.3126 -0.000477834 6.45819 0.514732 4.8483 1.48798C3.2384 2.46124 1.93649 3.85416 1.08813 5.51101L4.38775 8.02225C4.79132 6.82005 5.56974 5.77231 6.61327 5.02675C7.6568 4.28118 8.91279 3.87541 10.2042 3.86663Z"
+              fill="#EB4335"
+            />
+          </g>
+          <defs>
+            <clipPath id="clip0_95:967">
+              <rect width="20" height="20" fill="white" />
+            </clipPath>
+          </defs>
+        </svg>
+      </span>
+      Sign In with Google
+    </>
+  )}
+</button>
+
                 <div className="mb-8 flex items-center justify-center">
                   <span className="hidden h-[1px] w-full max-w-[70px] bg-body-color sm:block"></span>
                   <p className="md:text-md w-full px-5 text-center text-xs font-semibold text-gray-700 dark:text-white sm:text-sm">
